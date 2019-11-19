976c965cedfe5dbb53cf5c1c5e734fca5ff18c67
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-11-19 22:54:03 +0100

    Check if valueToSum is number, otherwise treat as nil

--- fbinstant/src/fbinstant.cpp
+++ fbinstant/src/fbinstant.cpp
@@ -905,7 +905,10 @@ static int FBInstant_LogEvent(lua_State* L) {
 	int top = lua_gettop(L);
 
 	const char* eventName = luaL_checkstring(L, 1);
-	const int valueToSum = luaL_checknumber(L, 2);
+	int valueToSum = 0;
+	if(lua_isnumber(L, 2)) {
+		valueToSum = luaL_checknumber(L, 2);
+	}
 	const char* parametersJson = luaL_checkstring(L, 3);
 	FBInstant_PlatformLogEvent(eventName, valueToSum, parametersJson);
 

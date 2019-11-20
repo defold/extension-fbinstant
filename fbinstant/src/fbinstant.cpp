3956e3802cc09a9bd97d9a142e24a9c126c64e24
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-11-20 17:13:04 +0100

    Handle if log_event contains nil params (#82)
    * Check if valueToSum is number, otherwise treat as nil
    
    * Also check if parameters is string or not

--- fbinstant/src/fbinstant.cpp
+++ fbinstant/src/fbinstant.cpp
@@ -905,8 +905,17 @@ static int FBInstant_LogEvent(lua_State* L) {
 	int top = lua_gettop(L);
 
 	const char* eventName = luaL_checkstring(L, 1);
-	const int valueToSum = luaL_checknumber(L, 2);
-	const char* parametersJson = luaL_checkstring(L, 3);
+	int valueToSum = 0;
+	if(lua_isnumber(L, 2)) {
+		valueToSum = luaL_checknumber(L, 2);
+	}
+	const char* parametersJson;
+	if (lua_isstring(L, 3)) {
+		parametersJson = luaL_checkstring(L, 3);
+	}
+	else {
+		parametersJson = "";
+	}
 	FBInstant_PlatformLogEvent(eventName, valueToSum, parametersJson);
 
 	assert(top == lua_gettop(L));
